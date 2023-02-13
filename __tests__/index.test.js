c8ae5596239b12ec62106f3536f86ee217f9c83e
j-goodman <johngoodman@pursuit.org>
2023-02-13 17:36:26 -0500

    Initial commit.

---
+++ __tests__/index.test.js
@@ -0,0 +1,155 @@
+const {
+  convertStringToInteger,
+  convertStringToNumber,
+  roundToInteger,
+  isItAnArray,
+  returnCountryNames,
+  returnCapitalCities,
+  returnCountriesAndCapitals,
+} = require("../");
+
+describe("convertStringToInteger()", () => {
+  test("should convert a string number to an integer number", () => {
+    const actual = convertStringToInteger("77");
+    const expected = 77;
+    expect(actual).toEqual(expected)
+  });
+
+  test("should convert strings with decimals to the next lowest integer", () => {
+    const actual = convertStringToInteger("1001.9");
+    const expected = 1001;
+    expect(actual).toEqual(expected)
+  });
+
+  test("should return NaN if the string is not parsable as an integer", () => {
+    const actual = convertStringToInteger("spaghetti");
+    const expected = NaN;
+    expect(actual).toEqual(expected)
+  });
+});
+
+describe("convertStringToNumber()", () => {
+  test("should convert a string number to an integer number", () => {
+    const actual = convertStringToNumber("77");
+    const expected = 77;
+    expect(actual).toEqual(expected)
+  });
+
+  test("should convert strings with decimals to the next lowest integer", () => {
+    const actual = convertStringToNumber("3.14159");
+    const expected = 3.14159;
+    expect(actual).toEqual(expected)
+  });
+
+  test(`should return "Not a number." if the string is not parsable as an integer`, () => {
+    const actual = convertStringToNumber("Phoenix, Arizona");
+    const expected = "Not a number.";
+    expect(actual).toEqual(expected)
+  });
+});
+
+describe("roundToInteger()", () => {
+  test("should round down if that instruction is given", () => {
+    const actual = roundToInteger(5.99, "down");
+    const expected = 5;
+    expect(actual).toEqual(expected)
+  });
+
+  test("should round up if that instructiuon is given", () => {
+    const actual = roundToInteger(5.01, "up");
+    const expected = 6;
+    expect(actual).toEqual(expected)
+  });
+
+  test(`should round to the nearest integer if no argument is given`, () => {
+    const actual = roundToInteger(5.51);
+    const expected = 6;
+    expect(actual).toEqual(expected)
+  });
+});
+
+describe("isItAnArray()", () => {
+  test("should return true if an array is given", () => {
+    const actual = isItAnArray([4, 8, 15, 16, 23, 42]);
+    const expected = true;
+    expect(actual).toEqual(expected)
+  });
+
+  test("should return false if a non-array is given", () => {
+    const actual = isItAnArray("Don Bluth");
+    const expected = false;
+    expect(actual).toEqual(expected)
+  });
+
+  test("should return false if a non-array is given", () => {
+    const actual = isItAnArray({A: 1, B: 2, C: 3});
+    const expected = false;
+    expect(actual).toEqual(expected)
+  });
+});
+
+const countriesObject = {
+  China: "Beijing",
+  India: "New Delhi",
+  "United States": "Washington, D.C.",
+  Indonesia: "Jakarta",
+  Pakistan: "Islamabad",
+}
+
+const alternateCountries = {
+  Nigeria: "Abuja",
+  Brazil: "Brasília",
+  Bangladesh: "Dhaka",
+}
+
+describe("returnCountryNames()", () => {
+  test("should return the given country names", () => {
+    const actual = returnCountryNames(countriesObject);
+    const expected = ["China", "India", "United States", "Indonesia", "Pakistan"];
+    expect(actual).toEqual(expected)
+  });
+
+  test("should work with other countries", () => {
+    const actual = returnCountryNames(alternateCountries);
+    const expected = ["Nigeria", "Brazil", "Bangladesh"];
+    expect(actual).toEqual(expected)
+  });
+});
+
+describe("returnCapitalCities()", () => {
+  test("should return the given country names", () => {
+    const actual = returnCapitalCities(countriesObject);
+    const expected = ["Beijing", "New Delhi", "Washington, D.C.", "Jakarta", "Islamabad"];
+    expect(actual).toEqual(expected)
+  });
+
+  test("should work with other countries", () => {
+    const actual = returnCapitalCities(alternateCountries);
+    const expected = ["Abuja", "Brasília", "Dhaka"];
+    expect(actual).toEqual(expected)
+  });
+});
+
+describe("returnCountriesAndCapitals()", () => {
+  test("should return the given country names", () => {
+    const actual = returnCountriesAndCapitals(countriesObject);
+    const expected = [
+      [ 'China', 'Beijing' ],
+      [ 'India', 'New Delhi' ],
+      [ 'United States', 'Washington, D.C.' ],
+      [ 'Indonesia', 'Jakarta' ],
+      [ 'Pakistan', 'Islamabad' ]
+    ];
+    expect(actual).toEqual(expected)
+  });
+
+  test("should work with other countries", () => {
+    const actual = returnCountriesAndCapitals(alternateCountries);
+    const expected = [
+      [ 'Nigeria', 'Abuja' ],
+      [ 'Brazil', 'Brasília' ],
+      [ 'Bangladesh', 'Dhaka' ]
+    ];
+    expect(actual).toEqual(expected)
+  });
+});
---
+++ index.js
@@ -0,0 +1,61 @@
+// Should convert a string representing a number into a number.
+// If the number is not an integer, round down to the nearest integer.
+// If the string cannot be parsed as a number, return NaN.
+function convertStringToInteger(string) {}
+
+// Should convert a string representing a number into a number.
+// Should work with integers or numbers with decimals.
+// If the string cannot be parsed as a number, return a string 
+// that says "Not a number.".
+function convertStringToNumber(string) {}
+
+// Should round a number to an integer. The instruction will be
+// one of the following strings:
+// "up" -- round up to the next highest integer
+// "down" -- round down to the next lowest integer
+// If no instruction is given, round to the nearest integer.
+function roundToInteger(number, instruction) {}
+
+// Returns true if the given argument is an array, or
+// false if it's not.
+function isItAnArray(item) {}
+
+// The next few functions use an object with the following shape:
+const countriesObject = {
+  China: "Beijing",
+  India: "New Delhi",
+  "United States": "Washington, D.C.",
+  Indonesia: "Jakarta",
+  Pakistan: "Islamabad",
+}
+
+// Should return an array of the given country names.
+function returnCountryNames(object) {}
+
+// Should return an array of the given country capital cities.
+function returnCapitalCities(object) {}
+
+// Should return an array of arrays, each containing a
+// country's name, and then that country's capital.
+// For example:
+/*
+[
+  [ 'China', 'Beijing' ],
+  [ 'India', 'New Delhi' ],
+  [ 'United States', 'Washington, D.C.' ],
+  [ 'Indonesia', 'Jakarta' ],
+  [ 'Pakistan', 'Islamabad' ]
+]
+*/
+function returnCountriesAndCapitals(object) {}
+
+// Don't change anything below this line.
+module.exports = {
+  convertStringToInteger,
+  convertStringToNumber,
+  roundToInteger,
+  isItAnArray,
+  returnCountryNames,
+  returnCapitalCities,
+  returnCountriesAndCapitals,
+};
